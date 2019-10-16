161601c84db6fd35cf9af487a7e555e72c5eef41
Eugenio Tampieri <eugenio@eutampieri.eu>
2019-10-16 10:33:42 +0200

    Created alias for Milano - Rho Fiera

--- src/mapping.rs
+++ src/mapping.rs
@@ -25,3 +25,33 @@ pub struct TrainTripLeg {
     pub categoriaDescrizione: String,
     pub numeroTreno: String,
 }
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct LFTrain {
+    pub trainidentifier: String,
+    pub trainacronym: String,
+    pub traintype: char,
+    pub pricetype: char,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct LFSolution {
+    pub idsolution: String,
+    pub origin: String,
+    pub destination: String,
+    pub direction: String,
+    pub departuretime: u64,
+    pub arrivaltime: u64,
+    pub minprice: f64,
+    pub optionaltext: Option<String>,
+    pub duration: String,
+    pub changesno: u8,
+    pub bookable: bool,
+    pub saleable: bool,
+    pub trainlist: Vec<LFTrain>,
+    pub onlycustom: bool,
+    pub extraInfo: Vec<String>,
+    pub showSeat: bool,
+    pub specialOffer: Option<f64>,
+    pub transportMeasureList: Vec<String>,
+}
