f04d977d908e69cc4b0ed576a0da010c4ff019ec
Eugenio Tampieri <eugenio@eutampieri.eu>
2019-10-16 18:12:55 +0200

    Added the ability to pick up from where it left

--- map_for_lefrecce.py
+++ map_for_lefrecce.py
@@ -4,16 +4,25 @@ from urllib.parse import quote_plus
 
 mapping = {}
 
+for row in open("vt_lf_map.tsv").read().split("\n"):
+    row_vals = row.split("\t")
+    mapping[row_vals[0]] = mapping[row_vals[1]]
+
 base_url = "https://www.lefrecce.it/msite/api/geolocations/locations?name="
 
 stations_list = [row.split("\t")[0] for row in open("stazioni_coord.tsv").read().replace("\r", '').split("\n")[1:]]
 
-for station in stations_list[:10]:
+for station in stations_list:
     print(station)
     found = False
     iters = 0
     while not found:
-        response = loads(urlopen(base_url + quote_plus(station[:-iters] if iters > 0 else station)).read())
+        try:
+            response = loads(urlopen(base_url + quote_plus(station.replace('`',"'")[:-iters] if iters > 0 else station)).read())
+        except:
+            with open("vt_lf_map.tsv", 'w') as f:
+                f.write('\n'.join([k+'\t'+v for k,v in mapping.items()]))
+            exit()
         #print(base_url + quote_plus(station[:-iters] if iters > 0 else station), response)
         if len(response) == 0:
             iters = iters + 1
