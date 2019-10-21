104f36111f33ecfd286e4694dbba36b4bbb940ca
Eugenio Tampieri <eugenio@eutampieri.eu>
2019-10-21 11:42:30 +0200

    Added new ID system

--- id_gen.py
+++ id_gen.py
@@ -1,6 +1,7 @@
 vt_list = [f.split('\t') for f in open("stazioni_coord.tsv").read().split("\n")[1:-1]]
 names = {}
 id_vt_map = {}
+vt_id_map = {}
 id_vtname_map = {}
 
 def encode(sid):
@@ -8,6 +9,15 @@ def encode(sid):
     for i,c in enumerate(sid):
         tot = tot + ((ord(c)-ord('A')) << i*5)
     return tot
+
+def permutations(sid):
+    return [
+        sid[0]+sid[2]+sid[1],
+        sid[1]+sid[0]+sid[2],
+        sid[1]+sid[2]+sid[0],
+        sid[2]+sid[0]+sid[1],
+        sid[2]+sid[1]+sid[0],
+    ]
 # tmp={}
 for station in vt_list:
     if station[1] in names:
@@ -17,7 +27,7 @@ for station in vt_list:
         names[station[1]] = station[0]
 
 for vt_id, name in names.items():
-    name_rep = name.upper().replace('-',' ').replace("`", '').replace('.', ' ').replace('  ',' ').replace('/',' ').split(' ')
+    name_rep = name.upper().replace('-',' ').replace("`", '').replace("'", '').replace('.', ' ').replace('  ',' ').replace('/',' ').split(' ')
     name_rep = [piece for piece in name_rep if piece != '']
     #print(name)
     first_name_len = max(1, 4-len(name_rep))
@@ -31,14 +41,31 @@ for vt_id, name in names.items():
     while station_id in id_vt_map and iter_num < len(name_rep[-1]):
         station_id = station_id[:-1] + name_rep[-1][-iter_num]
         iter_num = iter_num + 1
+    if station_id in id_vt_map:
+        for permutation in permutations(station_id):
+            if permutation not in id_vt_map:
+                station_id = permutation
+                break
+    if station_id in id_vt_map and len(name_rep) > 3:
+        for piece in name_rep[3:]:
+            if station_id[:-1] + piece[0] not in id_vt_map:
+                station_id = station_id[:-1] + piece[0]
+                break
     while station_id in id_vt_map:
         for letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
             for replacement in [station_id[:-1]+letter, station_id[0]+letter+station_id[2:], station_id[:-2]+2*letter, letter+station_id[1:]]:
                 if not replacement in id_vt_map:
                     station_id = replacement
     id_vt_map[station_id] = vt_id
+    vt_id_map[vt_id] = station_id
     id_vtname_map[station_id] = name
     #print(station_id[:4], first_name_len, name_rep[0], sorted(name_rep, key=len, reverse=True)[0][-min(4, len(station_id)):])
 
 for s_id, name in id_vtname_map.items():
     print(name+"\t"+s_id + "\t" + str(encode(s_id)))# + tmp[id_vt_map[s_id]])
+
+with open("stations.tsv", 'w') as f:
+    f.write('\n'.join(['\t'.join([r[0], vt_id_map[r[1]], r[2], r[3], r[4]]) for r in vt_list]))
+
+with open("id_vt.tsv", 'w') as f:
+    f.write('\n'.join(['\t'.join([k, v]) for k, v in id_vt_map.items()]))
--- map_for_lefrecce.py
+++ map_for_lefrecce.py
@@ -5,38 +5,36 @@ from urllib.parse import quote_plus
 mapping = {}
 mapping_vt_id = {}
 
-for row in open("stazioni_coord.tsv").read().replace("\r", '').split("\n")[1:-1]:
+for row in open("stations.tsv").read().replace("\r", '').split("\n"):
     data = row.split("\t")
     mapping_vt_id[data[0]] = data[1]
 
-for row in open("vt_lf_map.tsv").read().split("\n"):
+for row in open("id_lf_map.tsv").read().split("\n"):
     row_vals = row.split("\t")
     mapping[row_vals[0]] = row_vals[1]
 
 base_url = "https://www.lefrecce.it/msite/api/geolocations/locations?name="
 
-stations_list = [row.split("\t")[0] for row in open("stazioni_coord.tsv").read().replace("\r", '').split("\n")[1:-1]]
+stations_list = [row.split("\t") for row in open("stations.tsv").read().replace("\r", '').split("\n")]
 
 for station in stations_list:
-    print(station)
-    found = station in mapping
+    name = station[0]
+    print(name)
+    found = station[1] in mapping
     iters = 0
     while not found:
         try:
-            response = loads(urlopen(base_url + quote_plus(station.replace('`',"'")[:-iters] if iters > 0 else station)).read())
+            response = loads(urlopen(base_url + quote_plus(name.replace('`',"'")[:-iters] if iters > 0 else name)).read())
         except:
-            with open("vt_lf_map.tsv", 'w') as f:
+            with open("id_lf_map.tsv", 'w') as f:
                 f.write('\n'.join([k+'\t'+v for k,v in mapping.items()]))
             exit()
         #print(base_url + quote_plus(station[:-iters] if iters > 0 else station), response)
         if len(response) == 0:
             iters = iters + 1
         else:
-            mapping[station] = response[0]["name"]
+            mapping[station[1]] = response[0]["name"]
             found = True
 
-with open("vt_lf_map.tsv", 'w') as f:
+with open("id_lf_map.tsv", 'w') as f:
     f.write('\n'.join([k+'\t'+v for k,v in mapping.items()]))
-
-with open("lf_vt_map.tsv", 'w') as f:
-    f.write('\n'.join([v+'\t'+mapping_vt_id[k] for k,v in mapping.items()]))
